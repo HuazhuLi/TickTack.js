d71f86e89e2d66e85801f0d7b4af4dedcdc1e35e
HuazhuLi,李华柱 <huazhuhhfuufli@gmail.com>
2017-03-25 20:07:08 +0800

    Add files via upload

---
+++ resource.js
@@ -0,0 +1,32 @@
+let ticktack = {};
+
+
+function pageData () {
+	ticktack.pageData = shootNT2() || shootNT() || byMyself()
+};
+
+function shootNT() {
+	let data = window.performance.timing;
+
+	if (data.loadEventEnd - data.navigationStart < 0) {}
+		console.log("Page is still loading");
+
+	let index = {
+		url:window.location.href,
+		startMoment:data,navigationStart,
+	    dnsDuration: data.domainLookupEnd - data.domainLookupStart,
+	    tcpDuration: data.connectEnd - data.connectStart,
+	    total: data.loadEventEnd - data.navigationStart,
+	    timeToFirstByte: data.responseStart - data.navigationStart,
+	    domContentLoading: data.domContentLoadedEventStart - data.domLoading,
+	    domProcessing: data.domComplete - data.domLoading
+	}
+
+	return index;
+}
+
+
+function resourceData () {
+
+};
+
