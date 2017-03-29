54f89c79202cc2ecf9d5fe0b9cf45bd7ef73bbd2
HuazhuLi,李华柱 <huazhuhhfuufli@gmail.com>
2017-03-29 22:30:21 +0800

    Update PageDetail.js

--- src/tick-tack-Page/PageDetail.js
+++ src/tick-tack-Page/PageDetail.js
@@ -1,1 +1,31 @@
-var
+function pageDetail () {
+	ticktack.pageData.pageDetail = shootNT2() || shootNT() || byMyself()
+}
+
+function shootNT() {
+	let data = window.performance.timing;
+
+	if (data.loadEventEnd - data.navigationStart < 0) {}
+		console.log("Page is still loading");
+
+	let index = {
+		url:window.location.href,
+		startMoment:data,navigationStart,
+	    dnsDuration: data.domainLookupEnd - data.domainLookupStart,
+	    tcpDuration: data.connectEnd - data.connectStart,
+	    total: data.loadEventEnd - data.navigationStart,
+	    timeToFirstByte: data.responseStart - data.navigationStart,
+	    domContentLoading: data.domContentLoadedEventStart - data.domLoading,
+	    domProcessing: data.domComplete - data.domLoading
+	}
+
+	return index;
+}
+
+function shootNT2() {
+
+}
+
+function byMyself() {
+	
+}
